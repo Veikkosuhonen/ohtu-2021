e7ddad67ddc335b52f118ec24396710a842b8e32
Veikkosuhonen <Veikkosuhonen>
2021-11-22 15:12:53 +0200

    Tuotteen lisaamisen jälkeen korissa oikeanlainen ostosolio

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,22 +3,22 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self._ostokset = []
         pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.ostokset)
+        return len(self._ostokset)
 
     def hinta(self):
-        if self.ostokset:
-            return self.ostokset[0].hinta() * 2
+        if self._ostokset:
+            return self._ostokset[0].hinta() * 2
         return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         ostos = Ostos(lisattava)
-        self.ostokset.append(ostos)
+        self._ostokset.append(ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -29,6 +29,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self._ostokset
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,4 +26,12 @@ class TestOstoskori(unittest.TestCase):
     
     def test_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostos(self):
         self.kori.lisaa_tuote(Tuote("Maito", 5))
-        self.assertEqual(len(self.kori.ostokset), 1)
+        self.assertEqual(len(self.kori._ostokset), 1)
+    
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+ 
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual("Maito", ostos.tuote.nimi())
+        self.assertEqual(1, ostos.lukumaara())
